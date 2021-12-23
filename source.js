a2c42fe70dd4b3da2c5c1e71a94df62074247e21
Stephen Ramos <storamos@gmail.com>
2021-12-23 02:26:45 -0500

    Added rest api get and post request.

--- source.js
+++ source.js
@@ -15,3 +15,31 @@ function jumpToAboutMe() {
     document.querySelector("a[href='#About-me']").click();
 
 }
+
+async function fetchCount() {
+    let response = await fetch('https://api.lightwaves.me/api/visitor')
+    
+    return response.json();
+
+
+}
+async function addPageCount(){
+    const visitor = JSON.stringify({DateTime: (new Date()).toISOString(), UserAgent: navigator.userAgent, Width: document.documentElement.clientWidth, Height: document.documentElement.clientHeight })
+    const responseOptions = { 
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(visitor)
+    };
+    const postResp = await fetch("https://api.lightwaves.me", responseOptions);
+    const countObj = await fetchCount();
+    console.log(countObj["count"])
+    const footer = document.querySelector("#footer");
+    const node = document.createTextNode(`You are visitor number: ${countObj["count"]}`);
+    footer.appendChild(node);
+    
+
+
+
+}
+
+addPageCount();
